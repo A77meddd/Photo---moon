7608ccd028fb5dc9629c408fa39c0231d8b4d04c
A77meddd <alsbd338@gmail.com>
2025-04-13 14:41:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let light = document.getElementById('circle');
+let light = document.getElementById("circle");
+let eject = document.getElementById("photo");
+let button = document.getElementById("button");
 
-document.getElementById('button').addEventListener('click', () => {
-  light.classList.remove('flash-animation');
-  setTimeout(function(){
-  light.classList.add('flash-animation');
-  },500);
-});
+// مصفوفة الصور
+const images = ["Jerry.png", "Jerry1.png", "Jerry2.png", "Jerry3.png" ,"Moon.png"];
 
-let eject = document.getElementById('photo');
+let currentImageIndex = 0;
 
-document.getElementById('button').addEventListener('click', () => {
-  eject.classList.remove('eject-photo');
-  setTimeout(function(){
-  eject.classList.add('eject-photo');
-  },500);
+button.addEventListener("click", () => {
+  // تأثير الفلاش
+  light.classList.remove("flash-animation");
+  setTimeout(() => {
+    light.classList.add("flash-animation");
+  }, 100);
+
+  // تأثير إخراج الصورة
+  eject.classList.remove("eject-photo");
+  setTimeout(() => {
+    eject.classList.add("eject-photo");
+  }, 100);
+
+  // تغيير الخلفية بتاعة الصورة
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  eject.style.backgroundImage = `url('${images[currentImageIndex]}')`;
 });
